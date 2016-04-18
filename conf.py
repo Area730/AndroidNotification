ac8156d7930121c76c346895706c701019a322a9
Ярослав Пропой < >
2016-04-19 00:18:47 +0300

    v 1.5.1 is done

--- conf.py
+++ conf.py
@@ -30,11 +30,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	  'sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
